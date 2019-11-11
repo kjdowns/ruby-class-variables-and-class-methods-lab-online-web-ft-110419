7009c2f24327c457eb56d3bde24aed4e1a6798d8
Kevin Downs <kevin.downs04@gmail.com>
2019-11-11 22:57:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,6 +26,10 @@ class Song
   
   def self.genre_count
     genre_count = {}
+    @@genres.each do |genre|
+      genre_count[genre] ? genre_count[genre] += 1 : genre_count[genre] = 1
+    end
+    genre_count
   end
   
   def self.artists
